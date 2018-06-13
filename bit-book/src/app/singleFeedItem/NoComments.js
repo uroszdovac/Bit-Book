8075e6095383873ceaa3eccd984fa47f4ef53074
Dejan61 <datanackovic212@gmail.com>
2018-06-13 11:10:12 +0200

    feature-3-final

---
+++ bit-book/src/app/singleFeedItem/NoComments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoComments = () => (
+    <div id="noComments" className="col-4 offset-4">
+        <i class="fab fa-angellist"></i>
+        <p>
+            There is no comments on this post
+        </p>
+    </div>
+)
+export default NoComments;
