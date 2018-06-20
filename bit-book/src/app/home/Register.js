d67c1430bddc4e4bd2831184d62fe58746d91140
Dejan61 <datanackovic212@gmail.com>
2018-06-20 10:39:58 +0200

    register with one handler

--- bit-book/src/app/home/Register.js
+++ bit-book/src/app/home/Register.js
@@ -1,40 +1,40 @@
 import React from 'react';
 
-const Register = (props) => {
+class Register extends React.Component {
+    constructor(props) {
+        super(props)
 
-    const nameHandler = (event) => {
-        props.registerName(event.target.value)
+        this.state = {
+
+        }
+
+        this.handler = this.handler.bind(this);
+
     }
 
-    const emailHandler = (event) => {
-        props.registerEmail(event.target.value)
+    handler(event) {
+        console.log(event.target.name)
+
+        this.props.collectValues(event.target.value, event.target.name)
     }
 
-    const usernameHandler = (event) => {
-        props.registerUsername(event.target.value)
+    render() {
+        return (
+            <div>
+                <input className='col-12' type="text" id="registerName" name="registerName" placeholder=" Full Name" onChange={this.handler} />
+
+                <input className='col-12' type="email" id="registerEmail" name="registerEmail" placeholder="Email Address" onChange={this.handler} />
+
+                <input className='col-12' type="text" id="loginUsername" name="loginUsername" placeholder="Username" onChange={this.handler} />
+
+                <input className='col-12' type="password" id="registerPass" name="registerPass" placeholder="Password" onChange={this.handler} />
+                {this.props.passwordError ? <p className='error'>Minimum 6 characters!</p> : ''}
+                <input className='col-12 registerButton' type="button" value="Register" onClick={this.props.registerNewUser} />
+
+                {this.props.userExist ? <p className='error'>This user already exist!</p> : ''}
+
+            </div >
+        )
     }
-
-    const passwordHandler = (event) => {
-        props.registerPassword(event.target.value)
-    }
-
-
-    return (
-        <div>
-            <input className='col-12' type="text" id="registerName" name="registerName" placeholder=" Full Name" onChange={nameHandler} />
-
-            <input className='col-12' type="email" id="registerEmail" name="registerEmail" placeholder="Email Address" onChange={emailHandler} />
-
-            <input className='col-12' type="text" id="loginUsername" name="loginUsername" placeholder="Username" onChange={usernameHandler} />
-
-            <input className='col-12' type="password" id="registerPass" name="registerPass" placeholder="Password" onChange={passwordHandler} />
-            {props.passwordError ? <p class='error'>Minimum 6 characters!</p> : ''}
-            <input className='col-12 registerButton' type="button" value="Register" onClick={props.registerNewUser} />
-
-            {props.userExist ? <p class='error'>This user already exist!</p> : ''}
-
-        </div>
-    )
 }
-
 export default Register;
